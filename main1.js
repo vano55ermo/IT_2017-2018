976bd36ca4905aed598ec9f7a62647e0db3aa78d
vano55ermo <32243772+vano55ermo@users.noreply.github.com>
2017-11-19 13:14:16 +0300

    Update main1.js

--- main1.js
+++ main1.js
@@ -1,27 +1,26 @@
+var scene, camera, renderer, earth, mars, controls;
+
 class Body
 {
-	constructor(inputSize, inputColor, inputTrajectory)
-	{
-		this.size = inputSize;
-this.color = inputColor;
-this.trajectory = inputTrajectory;
+	constructor(size, color, trajectory) {
+		var geometry = new THREE.SphereGeometry(size, 8, 6);
+var material = new THREE.MeshBasicMaterial({color: color});
+
+this.threeObj = new THREE.Mesh(geometry, material);
+scene.add(this.threeObj);
+
+this.trajectory = trajectory;
 }
 
-render(time)
-{
+update(time) {
 	var pos = this.trajectory.getPosition(time);
 
-	context.beginPath();
-	context.arc(pos.x, pos.y, this.size, 0, 2 * Math.PI);
-	context.fillStyle = this.color;
-	context.fill();
-
-	this.trajectory.render(time);
+	this.threeObj.position.set(pos.x, pos.y, pos.z);
 }
 }
 
 // Родительский класс (абстрактный)
-class Trajectory
+class TrajectoryAbstract
 {
 	constructor(parentTrajectory)
 	{
@@ -29,13 +28,12 @@ class Trajectory
 }
 
 getPosition(time) {}
-render(time) {}
 }
 
-// Дочерний класс (наследник Trajectory)
-class StaticPosition extends Trajectory
+// Дочерний класс (наследник TrajectoryAbstract)
+class TrajectoryStaticPosition extends TrajectoryAbstract
 {
-	constructor(parentTrajectory, x, y)
+	constructor(parentTrajectory, x, y, z)
 {
 	// Вызов родительского конструктора
 // Метод constructor класса Trajectory
@@ -45,11 +43,12 @@ class StaticPosition extends Trajectory
 // для класса StaticPosition
 this.x = x;
 this.y = y;
+this.z = z;
 }
 
 getPosition(time)
 {
-	var parentPosition = {x: 0, y: 0};
+	var parentPosition = {x: 0, y: 0, z: 0};
 
 	if (this.parentTrajectory)
 {
@@ -58,12 +57,13 @@ getPosition(time)
 
 	return {
 x: parentPosition.x + this.x,
-y: parentPosition.y + this.y
+y: parentPosition.y + this.y,
+z: parentPosition.z + this.z
 };
 }
 }
 
-class CircleOrbit extends Trajectory
+class TrajectoryCircleOrbit extends TrajectoryAbstract
 {
 	constructor(parentTrajectory, radius, startingAngle, angularVelocity)
 	{
@@ -72,7 +72,7 @@ class CircleOrbit extends Trajectory
 		this.r = radius;
 		this.startingAngle = startingAngle;
 		this.angularVelocity = angularVelocity;
-}
+	}
 
 getPosition(time)
 {
@@ -81,36 +81,59 @@ getPosition(time)
 
 	return {
 		x: parentPosition.x + this.r * Math.cos(angle),
-		y: parentPosition.y + this.r * Math.sin(angle)
-};
-}
-
-render(time)
-{
-	var pos = this.parentTrajectory.getPosition(time);
-
-	context.beginPath();
-	context.arc(pos.x, pos.y, this.r, 0, 2 * Math.PI);
-	context.strokeStyle = "grey";
-	context.lineWidth = 2;
-	context.stroke();
+		y: parentPosition.y + this.r * Math.sin(angle),
+		z: 0
+			};
 }
 }
 
-function update()
-{
-	time = time + 0.1;
+// Инициализирует сцену (1 раз)
+function init() {
+scene = new THREE.Scene();
+camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+camera.up = new THREE.Vector3(0, 0, 1);
+camera.position.z = 25;
+
+
+renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
+
+controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+scene.add(new THREE.AxisHelper(3));
+}
+
+// Создаёт объекты (1 раз)
+function initObjects() {
+	var sunPosition = new TrajectoryStaticPosition(null, 0, 0, 0);
+	var earthOrbit = new TrajectoryCircleOrbit(sunPosition, 10, 0, 0.2);
+	var marsOrbit = new TrajectoryCircleOrbit(sunPosition, 15, 0, 0.1);
 	
-	context.clearRect(0, 0, 1500, 900);
-	earth.render(time);
-	sun.render(time);
+	sun = new Body(2,'yellow',sunPosition);
+	earth = new Body(1, 'blue', earthOrbit);
+	mars = new Body(0.5, 'red', marsOrbit);
 }
 
-var time = 0;
-var sunTrajectory = new StaticPosition(null, 500, 400);
-var earthTrajectory = new CircleOrbit(sunTrajectory, 300, 0, 1);
-var context = document.getElementById("mainCanvas").getContext("2d");
-var earth = new Body(40, "blue", earthTrajectory);
-var sun = new Body(100,"yellow",sunTrajectory);
+// Обновляет объекты (на каждом кадре)
+function updateObjects(time) {
+	earth.update(time)
+	mars.update(time)
+}
 
-setInterval(update, 50);
+// Отрисовка сцены на каждом кадре
+function render(time) {
+updateObjects(time / 1000);
+
+controls.update();
+
+	renderer.render(scene, camera);
+			requestAnimationFrame(render);
+	};
+
+init();
+initObjects();
+
+
+requestAnimationFrame(render);
