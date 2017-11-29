bb314da8168db2a48e7439214f62fc19844e6d80
vano55ermo <32243772+vano55ermo@users.noreply.github.com>
2017-11-29 10:47:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -293,7 +293,7 @@ function init()
 	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 	camera.up = new THREE.Vector3(0, 0, 1);
-	camera.position.z = 25;
+	camera.position.z = 500;
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
